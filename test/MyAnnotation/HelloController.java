70711762e2496add9cddbac8c22250542faec373
NyEjaBoum <razanatsimbanyeja@gmail.com>
2025-11-10 09:24:40 +0300

    sprint 4 test

--- test/MyAnnotation/HelloController.java
+++ test/MyAnnotation/HelloController.java
@@ -2,6 +2,7 @@ package MyAnnotation;
 
 import framework.annotation.Controller;
 import framework.annotation.MyAnnotation;
+import framework.view.ModelView;
 
 @Controller(value = "/hello")
 public class HelloController {
@@ -9,4 +10,20 @@ public class HelloController {
     public void sayHello() {
         System.out.println("HelloController.sayHello()");
     }
+
+    @MyAnnotation(value = "/getString")
+    public String getString() {
+        System.out.println("HelloController.getString()");
+        return "Bonjour depuis HelloController!";
+    }
+
+    // retourne ModelView -> forward vers JSP
+    @MyAnnotation(value = "/getView")
+    public ModelView getView() {
+        System.out.println("HelloController.getView()");
+        ModelView mv = new ModelView();
+        mv.setView("WEB-INF/test.jsp"); // ou "test.jsp" si JSP en racine webapp
+        return mv;
+    }
+
 }
